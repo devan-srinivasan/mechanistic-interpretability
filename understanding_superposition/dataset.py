061ca985d082d1d1588835fa524055496530d3ce
Devan Srinivasan <devan.srinivasan@mail.utoronto.ca>
2025-10-05 10:11:48 -0400

    adding data

--- understanding_superposition/dataset.py
+++
@@ -1,6 +0,0 @@
-"""
-Generate dataset for modelling features of embeddings 
-
-Can start with English dictionary embeddings (like 35k)
-Define some parquet or arrow file for these. Ideally pick some embeddings with smaller dimension
-"""
