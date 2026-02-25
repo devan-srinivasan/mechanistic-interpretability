516c31b5f4ae1297e3016e386f459c1c0ec9d511
Devan Srinivasan <devan.srinivasan@mail.utoronto.ca>
2026-02-25 09:11:38 -0500

    trying to debug OOM

--- understanding_superposition/playground.py
+++ understanding_superposition/playground.py
@@ -1,11 +1,29 @@
-from datasets import load_dataset
+# from datasets import load_dataset
+# import torch
+
+# # Load the WikiText dataset with streaming enabled
+# # dataset = load_dataset("wikitext", "wikitext-103-v1", split="train", streaming=True)
+
+# encoder = torch.nn.Linear(768, 64)
+
+# rand = torch.ones((5, 512, 768))
+
+# print(encoder(rand).shape)
+
+from sae import SAE
 import torch
 
-# Load the WikiText dataset with streaming enabled
-# dataset = load_dataset("wikitext", "wikitext-103-v1", split="train", streaming=True)
+input = torch.randn((64, 512, 768))
 
-encoder = torch.nn.Linear(768, 64)
+model = SAE(embed_dim=768, hidden_dim=768 * 50)
 
-rand = torch.ones((5, 512, 768))
+optim = torch.optim.Adam(model.parameters(), lr=1e-3)
 
-print(encoder(rand).shape)
+reconstructed, codes = model(input)
+
+loss = torch.nn.functional.mse_loss(reconstructed, input)
+
+loss.backward()
+
+optim.step()
+
